010714db58b9f7b667c748f52d740f151b4449d7
ricky_jian <rjricky03bc@gmail.com>
2017-11-13 14:56:55 +0800

    top update
    if user does not login , he will see login and register.
    if user login , he will see logout , register , and menu

--- roadtoadventure/WebContent/assets/js/top.js
+++ roadtoadventure/WebContent/assets/js/top.js
@@ -1,11 +1,20 @@
-var topHTML ="<nav class=\"light-blue lighten-1\" role=\"navigation\">"
-	      +"<div class=\"nav-wrapper container\"><a id=\"logo-container\" href=\""+contextPath+"/\" class=\"brand-logo\">Logo</a>"
-	        +"<ul class=\"right hide-on-med-and-down\">"
-	          +"<li><a href=\""+contextPath+"/SignUp\">註冊</a></li>"
-	          +"<li><a href=\""+contextPath+"/Login\">登入</a></li>"
-	          +"<li><a href=\""+contextPath+"/j_spring_security_logout.jsp\">登出</a></li>"
-	          +"<li><a id=\"menu\" data-activates=\"slide-out\" ><i class=\"material-icons\">menu</i></a></li>"
-	        +"</ul>"
-	     +"</div>"
-	   +"</nav>" 
+var topHTML ="";
+appendHTML()
+function appendHTML(){
+  topHTML ="<nav class=\"light-blue lighten-1\" role=\"navigation\">"
+  topHTML +="<div class=\"nav-wrapper container\"><a id=\"logo-container\" href=\""+contextPath+"/\" class=\"brand-logo\">Logo</a>"
+  topHTML +="<ul class=\"right hide-on-med-and-down\">"
+  topHTML +="<li><a href=\""+contextPath+"/SignUp\">註冊</a></li>"
+  if(user=="anonymousUser"){
+    topHTML +="<li><a href=\""+contextPath+"/Login\">登入</a></li>"
+  }else{
+	topHTML +="<li><a href=\""+contextPath+"/j_spring_security_logout.jsp\">登出</a></li>"
+    topHTML +="<li><a id=\"menu\" data-activates=\"slide-out\" ><i class=\"material-icons\">menu</i></a></li>"
+  }
+	topHTML +="</ul>"
+	topHTML +="</div>"
+	topHTML +="</nav>" 
+}
+
+
 document.write(topHTML)
